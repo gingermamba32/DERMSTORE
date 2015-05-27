77dbab468e589c2ef4b02ef615bb31f7ab4565cf
Michael_Montero <mmontero2012@gmail.com>
2015-05-26 23:46:59 -0700

    good changes to button media queries

--- index.php
+++ index.php
@@ -1,1 +1,3 @@
 <?php include_once("index.html"); ?>
+
+<!-- for heroku build...Bootstrap discrepancies in build between Css/js CDN vs Dermstore CDN -->
--- js/script.js
+++ js/script.js
@@ -1,25 +1,71 @@
-		
-			$( "#tool-one" ).hover(
+			// tool-kit for product 1/4 **************
+			$( ".tool-one" ).hover(
 			  function() {
-			    $( this ).append( $( "<span>******</span>" ) );
+			    $( this ).append( $( "<span>Herbal Recovery Oil</span>" ) );
 			  }, function() {
 			    $( this ).find( "span:last" ).remove();
 			  }
 			);
- 
-			$( "#tool-one.fade" ).hover(function() {
+			$( ".tool-one.fade" ).hover(function() {
+			  $( this ).fadeOut( 100 );
+			  $( this ).fadeIn( 500 );
+			});
+			// tool-kit for product 2/4 **************
+			$( ".tool-two" ).hover(
+			  function() {
+			    $( this ).append( $( "<span>Facial Brush</span>" ) );
+			  }, function() {
+			    $( this ).find( "span:last" ).remove();
+			  }
+			);
+			$( ".tool-one.two" ).hover(function() {
+			  $( this ).fadeOut( 100 );
+			  $( this ).fadeIn( 500 );
+			});
+			// tool-kit for product 3/4 *************
+			$( ".tool-three" ).hover(
+			  function() {
+			    $( this ).append( $( "<span>Hand Moisturizer</span>" ) );
+			  }, function() {
+			    $( this ).find( "span:last" ).remove();
+			  }
+			);
+			$( ".tool-three.fade" ).hover(function() {
+			  $( this ).fadeOut( 100 );
+			  $( this ).fadeIn( 500 );
+			});
+			// toolkit for product 4/4 ***************
+			$( ".tool-four" ).hover(
+			  function() {
+			    $( this ).append( $( "<span>Beauty Ingenuity</span>" ) );
+			  }, function() {
+			    $( this ).find( "span:last" ).remove();
+			  }
+			);
+			$( ".tool-four.fade" ).hover(function() {
 			  $( this ).fadeOut( 100 );
 			  $( this ).fadeIn( 500 );
 			});
 
+
+			// Slide-Up animation for the header ********
 			$(function() {
 			    $("h1").animate(
-			        {bottom : "+=60"}, 1000, function() {}
+			        {bottom : "+=70"}, 1900, function() {}
 			    );
 			});
+
+			// Fade-In for the Grid display 
 			$(function(){
 				$(".row").animate(
-				{opacity: 1}, 2000, function(){}
+				{opacity: 1}, 2500, function(){}
+				);
+			})
+
+			// Fade-In for the banner
+			$(function(){
+				$(".main-header").animate(
+				{opacity: 1}, 2500, function(){}
 				);
 			})
 
